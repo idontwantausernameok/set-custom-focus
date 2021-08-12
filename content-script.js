7eac3d197e2fe37618206994625f98800275d083
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-08-12 18:56:30 +0200

    inital

---
+++ content-script.js
@@ -0,0 +1,56 @@
+
+(async () => {
+
+	if (typeof window.setfocus_hasRun !== 'undefined'){
+		return;
+	}
+	window.setfocus_hasRun = true;
+
+	let store = {};
+
+	try {
+		store = await browser.storage.local.get('selectors');
+	}catch(e){
+		console.error('error', 'access to rules storage failed');
+		return;
+	}
+
+	if ( typeof store.selectors !== 'object' ) { 
+		console.error('error', 'rules selectors not available');
+		return;
+	}
+
+	if ( typeof store.selectors.forEach !== 'function' ) { 
+		console.error('error', 'rules selectors not iterable');
+		return;
+	}
+
+	store.selectors.forEach( (selector) => {
+
+		// check activ
+		if(typeof selector.activ !== 'boolean') { return; }
+		if(selector.activ !== true) { return; }
+
+		// check url regex 
+		if(typeof selector.url_regex !== 'string') { return; }
+		selector.url_regex = selector.url_regex.trim(); 
+		if(selector.url_regex === ''){ return; }
+
+		if(!(new RegExp(selector.url_regex)).test(window.location.href)){ return; }
+
+		if ( typeof selector.code !== 'string' ) { return; }
+		if ( selector.code === '' ) { return; }
+
+		try {
+			setTimeout(function() {
+				const item = document.querySelector(selector.code);
+					if( typeof item.focus === 'function') {
+						item.focus(); // click item 
+					}
+			}, selector.delay || 3000); // wait delay
+		}catch(e){
+			console.error(e);	
+		}
+	});
+
+})();
